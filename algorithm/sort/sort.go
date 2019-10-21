01fd92f868ffa348f36ed2bb4eb7474f4269312b
stirling <stirling.li@nf-3.com>
2019-10-21 21:13:43 +0800

    implement quick sort

--- algorithm/sort/sort.go
+++ algorithm/sort/sort.go
@@ -3,13 +3,61 @@ package sort
 import (
 	"github.com/liyue201/gostl/uitls/comparator"
 	"github.com/liyue201/gostl/uitls/iterator"
-) 
 
+	"math/rand"
+)   
+              
 //sort the container by using quick sort
 func Sort(begin, end iterator.SortableIterator, cmp comparator.Comparator) {
 	quickSort(begin, end, cmp)
+}        
+          
+func quickSort(begin, end iterator.SortableIterator, cmp comparator.Comparator) {
+	if begin.Position()+1 >= end.Position() {
+		return
+	}     
+
+	len := end.Position() - begin.Position()
+	pos := rand.Int()%len + begin.Position()
+	baseItem := begin.IteratorAt(pos)
+	baseValue := baseItem.Value()
+	if baseItem.Position() != begin.Position() {
+		swapValue(baseItem, begin)
+	}  
+
+	leftIter := (begin.Clone().(iterator.SortableIterator).Next()).(iterator.SortableIterator)
+	rightIter := (begin.Clone().(iterator.SortableIterator)).IteratorAt(begin.Position() + len - 1)
+	for leftIter.Position() < rightIter.Position() {
+		leftCmp := cmp(leftIter.Value(), baseValue)
+		if leftCmp <= 0 {
+			leftIter.Next()
+		} else {
+			rightCmp := cmp(rightIter.Value(), baseValue)
+			if rightCmp >= 0 {
+				rightIter.Prev()
+			} else {
+				swapValue(leftIter, rightIter)
+				leftIter.Next()
+				if leftIter.Position() < rightIter.Position() {
+					rightIter.Prev()
+				}
+			}
+		}
+	}
+
+	if cmp(leftIter.Value(), begin.Value()) < 0 {
+		swapValue(begin, leftIter)
+		quickSort(begin, leftIter.Clone().(iterator.SortableIterator), cmp)
+		quickSort(leftIter.Clone().(iterator.SortableIterator).Next().(iterator.SortableIterator), end, cmp)
+	}else {
+		quickSort(begin, leftIter.Clone().(iterator.SortableIterator), cmp)
+		quickSort(leftIter.Clone().(iterator.SortableIterator).(iterator.SortableIterator), end, cmp)
+	}
 }
 
-func quickSort(begin, end iterator.SortableIterator, cmp comparator.Comparator) {
-	//todo
+func swapValue(a, b iterator.SortableIterator) {
+	valA := a.Value()
+	valB := b.Value()
+	a.SetValue(valB)
+	b.SetValue(valA)
 }
---
+++ algorithm/sort/sort_test.go
@@ -0,0 +1,88 @@
+package sort
+
+import (
+	"github.com/liyue201/gostl/containers/vector"
+	"github.com/liyue201/gostl/uitls/comparator"
+	"math/rand"
+	"testing"
+	"time"
+)
+       
+func TestSort(t *testing.T) {
+	// test size = 0
+	rand.Seed(time.Now().UnixNano())
+	v := vector.New(0)
+	Sort(v.Begin(), v.End(), comparator.BuiltinTypeComparator)
+	t.Logf("v: %v", v.String())
+
+	// test size = 1
+	v = vector.New(0)
+	for i := 0; i< 1 ; i++ {
+		v.PushBack(rand.Int() % 10)
+	}
+	Sort(v.Begin(), v.End(), comparator.BuiltinTypeComparator)
+
+	t.Logf("v: %v", v.String())
+	for i := 0; i < v.Size() - 1; i++ {
+		if v.At(i).(int) > v.At(i + 1).(int) {
+			t.Fatalf("sort vector error")
+		}
+	}
+
+	// test size = 2
+	v = vector.New(0)
+	for i := 0; i< 2; i++ {
+		v.PushBack(rand.Int() % 10)
+	}
+	Sort(v.Begin(), v.End(), comparator.BuiltinTypeComparator)
+
+	t.Logf("v: %v", v.String())
+	for i := 0; i < v.Size() - 1; i++ {
+		if v.At(i).(int) > v.At(i + 1).(int) {
+			t.Fatalf("sort vector error")
+		}
+	}
+
+	// test size = 10
+	v = vector.New(0)
+	for i := 0; i< 10; i++ {
+		v.PushBack(rand.Int() % 10)
+	}
+	Sort(v.Begin(), v.End(), comparator.BuiltinTypeComparator)
+
+	t.Logf("v: %v", v.String())
+	for i := 0; i < v.Size() - 1; i++ {
+		if v.At(i).(int) > v.At(i + 1).(int) {
+			t.Fatalf("sort vector error")
+		}
+	}
+
+	// test size = 31
+	v = vector.New(0)
+	for i := 0; i< 31; i++ {
+		v.PushBack(rand.Int() % 10)
+	}
+	Sort(v.Begin(), v.End(), comparator.BuiltinTypeComparator)
+
+	t.Logf("v: %v", v.String())
+	for i := 0; i < v.Size() - 1; i++ {
+		if v.At(i).(int) > v.At(i + 1).(int) {
+			t.Fatalf("sort vector error")
+		}
+	}
+
+
+	// test size = 50
+	v = vector.New(0)
+	for i := 0; i< 50; i++ {
+		v.PushBack(rand.Int() % 100)
+	}
+	Sort(v.Begin(), v.End(), comparator.BuiltinTypeComparator)
+
+	t.Logf("v: %v", v.String())
+	for i := 0; i < v.Size() - 1; i++ {
+		if v.At(i).(int) > v.At(i + 1).(int) {
+			t.Fatalf("sort vector error")
+		}
+	}
+}
