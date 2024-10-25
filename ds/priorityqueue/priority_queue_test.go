80828f40d32b91e5645307821605964475294b36
Yiwei Zhou <flamingo@theplant.jp>
2024-10-25 14:48:34 +0800

    fix: update min/max priority queue tests

--- ds/priorityqueue/priority_queue_test.go
+++ ds/priorityqueue/priority_queue_test.go
@@ -18,8 +18,32 @@ func TestMinPriorityQueue(t *testing.T) {
 	}
 }
 
-func TestMaxPriorityQueue(t *testing.T) {
+func TestMinPriorityQueueWithGoroutineSafe(t *testing.T) {
 	pq := New(comparator.IntComparator, WithGoroutineSafe())
+	pq.Push(4)
+	pq.Push(8)
+	pq.Push(1)
+	pq.Push(6)
+	pq.Push(3)
+	for !pq.Empty() {
+		t.Logf("%v,%v", pq.Top(), pq.Pop())
+	}
+}
+
+func TestMaxPriorityQueue(t *testing.T) {
+	pq := New(comparator.Reverse(comparator.IntComparator))
+	pq.Push(4)
+	pq.Push(8)
+	pq.Push(1)
+	pq.Push(6)
+	pq.Push(3)
+	for !pq.Empty() {
+		t.Logf("%v, %v", pq.Top(), pq.Pop())
+	}
+}
+
+func TestMaxPriorityQueueWithGoroutineSafe(t *testing.T) {
+	pq := New(comparator.Reverse(comparator.IntComparator), WithGoroutineSafe())
 	pq.Push(4)
 	pq.Push(8)
 	pq.Push(1)
